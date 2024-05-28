c841d4a00857491fc5201d39dff049f23399c06c
BoyAtom <glebstarin@yandex.ru>
2024-05-28 09:24:03 +0300

    Updated DB

--- GEOData.sql
+++ GEOData.sql
@@ -1,22 +1,25 @@
 --
--- Файл сгенерирован с помощью SQLiteStudio v3.4.4 в Чт май 16 19:12:02 2024
+-- Файл сгенерирован с помощью SQLiteStudio v3.4.4 в Вт май 28 09:23:39 2024
 --
 -- Использованная кодировка текста: System
 --
 PRAGMA foreign_keys = off;
 BEGIN TRANSACTION;
 
+-- Таблица: Colors
+CREATE TABLE IF NOT EXISTS Colors (Enterprise INTEGER REFERENCES Enterprises (Key), Red INTEGER DEFAULT (0), Green INTEGER DEFAULT (0), Blue INTEGER DEFAULT (0));
+
 -- Таблица: Company
-CREATE TABLE Company (Key INTEGER PRIMARY KEY AUTOINCREMENT, Name TEXT NOT NULL, Description TEXT);
+CREATE TABLE IF NOT EXISTS Company (Enterprise INTEGER REFERENCES Enterprises (Key), Name TEXT NOT NULL, Description TEXT);
 
 -- Таблица: Enterprises
-CREATE TABLE Enterprises (Key INTEGER PRIMARY KEY AUTOINCREMENT, Company INTEGER REFERENCES Company (Key), Industry INTEGER REFERENCES Industries (Key), Name TEXT NOT NULL, Description TEXT);
+CREATE TABLE IF NOT EXISTS Enterprises (Key INTEGER PRIMARY KEY AUTOINCREMENT, Name TEXT NOT NULL, Description TEXT);
 
 -- Таблица: Industries
-CREATE TABLE Industries (Key INTEGER PRIMARY KEY AUTOINCREMENT, Name TEXT NOT NULL);
+CREATE TABLE IF NOT EXISTS Industries (Enterprise INTEGER REFERENCES Enterprises (Key), Name TEXT NOT NULL);
 
 -- Таблица: Tags
-CREATE TABLE Tags (Key INTEGER PRIMARY KEY AUTOINCREMENT, Enterprise INTEGER REFERENCES Enterprises (Key), X REAL NOT NULL, Y REAL NOT NULL);
+CREATE TABLE IF NOT EXISTS Tags (Enterprise INTEGER REFERENCES Enterprises (Key), X REAL NOT NULL, Y REAL NOT NULL);
 
 COMMIT TRANSACTION;
 PRAGMA foreign_keys = on;

720057f5aaf67e34acf01da56be4a569af825aae
BoyAtom <glebstarin@yandex.ru>
2024-06-13 22:55:13 +0300

    Updated application

--- Assets/Scripts/UsersSettings/UsersSettings.cs
+++ Assets/Scripts/UsersSettings/UsersSettings.cs
@@ -12,6 +12,7 @@ public class UsersSettings : MonoBehaviour
     TMP_InputField username_field;
     [SerializeField]
     TMP_InputField password_field;
+    List<string> id = new List<string>();
     List<string> names = new List<string>();
     List<string> passwords = new List<string>();
     List<string> roles = new List<string>();
@@ -26,12 +27,14 @@ public class UsersSettings : MonoBehaviour
 
     [System.Obsolete]
     void InitUsersData() {
+        id.Clear();
         names.Clear();
         passwords.Clear();
         roles.Clear();
         DataBases.DataBase.InitDatabasePath(DBName);
         DataTable users = DataBases.DataBase.GetTable("SELECT * FROM Users");
         foreach (DataRow row in users.Rows) {
+            id.Add(row["ID"].ToString());
             names.Add(row["Username"].ToString());
             passwords.Add(row["Password"].ToString());
             roles.Add(row["Role"].ToString());
@@ -43,6 +46,7 @@ public class UsersSettings : MonoBehaviour
 
     public void OnUserChange() {
         int current_user = users_dropdown.value;
+        print(current_user);
         username_field.text = names[current_user];
         password_field.text = passwords[current_user];
     }
@@ -51,12 +55,20 @@ public class UsersSettings : MonoBehaviour
         DataBases.DataBase.InitDatabasePath(DBName);
         DataBases.DataBase.ExecuteQueryWithoutAnswer(string.Format("INSERT INTO Users(Username, Password, Role) VALUES ('{0}', '{1}', '{2}')", ""+Random.Range(100000, 1000000), "", "1"));
         InitUsersData();
+        users_dropdown.value = id.Count-1;
+    }
+
+    public void DeleteUser() {
+        DataBases.DataBase.InitDatabasePath(DBName);
+        DataBases.DataBase.ExecuteQueryWithoutAnswer(string.Format("DELETE FROM Users WHERE ID = ('{0}')", id[users_dropdown.value]));
+        InitUsersData();
     }
 
     public void ChangeUserData() {
         DataBases.DataBase.InitDatabasePath(DBName);
-        DataBases.DataBase.ExecuteQueryWithoutAnswer(string.Format("REPLACE INTO Users(ID, Username, Password) VALUES ('{0}', '{1}', '{2}')", users_dropdown.value, username_field.text, password_field.text));
+        DataBases.DataBase.ExecuteQueryWithoutAnswer(string.Format("REPLACE INTO Users(ID, Username, Password) VALUES ('{0}', '{1}', '{2}')", id[users_dropdown.value], username_field.text, password_field.text));
         InitUsersData();
+        users_dropdown.value = id.Count-1;
     }
 
     void CheckDB(){
